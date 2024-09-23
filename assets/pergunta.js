0075ca452b85819d01b41242987702083ecf0c49
Analezinski <145598090+Analezinski@users.noreply.github.com>
2024-09-23 11:49:23 -0300

    sss

--- assets/pergunta.js
+++ assets/pergunta.js
@@ -7,13 +7,13 @@ criaCartao(
 criaCartao(
     'Disney',
     'Aonde fica o parque original da Disney?',
-    'Fica em Anaheim,na califórnia'
+    'Fica em Anaheim,na Califórnia'
 )
 
 criaCartao(
-    'Programação',
-    'O que é uma função?',
-    'Uma função é um bloco de código que executa alguma tarefa'
+    'Disney',
+    'Qual era o nome da mulher que queria roubar os 101 Dalmatas?',
+    'O nome dela era Cruela de Vil'
 )
 
 criaCartao(
